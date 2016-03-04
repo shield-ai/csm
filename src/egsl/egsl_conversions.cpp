8d3d65557d02e91eddf5b9fa16db0e6970c2fed5
Kenny Yokoyama <kenny.yokoyama1701@gmail.com>
2016-03-03 21:12:36 -0800

    One more file

--- src/egsl/egsl_conversions.cpp
+++ src/egsl/egsl_conversions.cpp
@@ -19,6 +19,43 @@ val egsl_vFa(size_t rows, const double*a) {
 	return v;
 }
 
+//void egsl_v2a(val v, double*vec) {
+//	gsl_matrix *m = egsl_gslm(v);
+//	size_t i;
+//	for(i=0; i < m->size1; i++)
+//		vec[i] = gsl_matrix_get(m,i,0);
+//}
+
+//void egsl_v2da(val v, double*a){
+//	gsl_matrix *m = egsl_gslm(v);
+//	size_t i,j;
+//	for(i=0;i<m->size1;i++)
+//		for(j=0;j<m->size2;j++)
+//			a[j*m->size1 +i] = gsl_matrix_get(m,i,j);
+//}
+
+//void egsl_v2vec(val v, gsl_vector*vec) {
+//	size_t i;
+//	for(i=0;i<vec->size;i++)
+//		gsl_vector_set(vec,i, *egsl_atmp(v,i,0));
+//}
+
+
+//val egsl_vFgslv(const gsl_vector*vec){
+//	val v = egsl_alloc(vec->size,1);
+//	size_t i;
+//	for(i=0;i<vec->size;i++)
+//		*egsl_atmp(v,i,0) = gsl_vector_get(vec,i);
+//	return v;
+//}
+
+//val egsl_vFgslm(const gsl_matrix*m){
+//	val v = egsl_alloc(m->size1,m->size2);
+//	gsl_matrix * m2 = egsl_gslm(v);
+//	gsl_matrix_memcpy(m2,m);
+//	return v;
+//}
+
 gsl_matrix* egsl_v2gslm(val v){
 	gsl_matrix * m = egsl_gslm(v); 
 	gsl_matrix * m2 = gsl_matrix_alloc(m->rows(),m->cols());
